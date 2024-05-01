7248cd2fa7a14a9bb01e872f25bf02256daac3c0
wadzz23 <wadzz23@outlook.com>
2024-05-01 18:05:27 -0300

    Calculadora

---
+++ Calculadora/src/comentarios.java
@@ -0,0 +1,23 @@
+public class comentarios {
+
+    public static void main(String[] args ) {
+        // Olá, eu sou um comentário
+
+        /* Olá, 
+         * EU sou um comentário 
+         * que posso ser mais detalhada quando
+         * necessário
+         */
+
+         
+
+
+    }
+    /** 
+          * Estas duas estrelinahs acima 
+          é para identificar que você pretende 
+          elaborar um comentário
+          a nível de documentação
+          
+          */
+}
