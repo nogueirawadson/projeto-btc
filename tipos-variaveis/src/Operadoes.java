1ed6558228f320c3ea506e1dd33e1986840edc65
wadzz23 <wadzz23@outlook.com>
2024-05-01 13:05:34 -0300

    Operadores Java

---
+++ tipos-variaveis/src/Operadoes.java
@@ -0,0 +1,16 @@
+public class Operadoes {
+
+        public static void main(String[] args) {
+            int numero1 = 1;
+            int numero2 = 2;
+    
+            boolean simNao = numero1 == numero2;
+    
+            System.out.println(" O numero: " + numero1 + " é igual a0 numero: " + numero2  + "? " + simNao);
+    
+    
+        }
+    
+    }
+    
+
