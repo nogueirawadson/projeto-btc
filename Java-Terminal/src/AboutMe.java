df830e437516455b65cc81cfbc9d6f5bc1c6fd1f
wadzz23 <wadzz23@outlook.com>
2024-05-02 23:59:09 -0300

    AboutMe

--- Java-Terminal/src/AboutMe.java
+++ Java-Terminal/src/AboutMe.java
@@ -1,4 +1,4 @@
-public class AboutMe {
+/** public class AboutMe {
 
     public static void main(String[] args) {
 
@@ -13,5 +13,43 @@ public class AboutMe {
 
 
     }
-    
+
 }
+
+**/
+import java.util.Locale;
+import java.util.Scanner;
+
+public class AboutMe {
+
+    public static void main(String[] args) {
+    
+        
+        // criando objeto scanner
+        
+       Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println("Digite sua altura: ");
+          double altura = scanner.nextDouble();
+1
+        System.out.println("Digite seu nome: ");
+        String nome = scanner.next();
+
+        System.out.println(" Digite seu Sobrenome: ");
+        String sobrenome = scanner.next();
+
+        System.out.println(" Digite sua idade: ");
+        int idade = scanner.nextInt();
+
+
+        // imprimindo dados obitidos
+        System.out.println("Olá me chamo" + nome + " " + sobrenome);
+        System.out.println("Tenho " + idade +  " anos");
+        System.out.println("Minha altura é: " + altura + " cm");
+
+
+
+    
+    }
+}
+
