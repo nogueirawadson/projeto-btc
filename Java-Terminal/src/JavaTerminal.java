75dafa9bd0308e34659ba1aec25c2f37260f589b
nogueirawadson <wadzz23@outlook.com>
2024-05-23 23:12:24 -0300

    nova

--- Java-Terminal/src/JavaTerminal.java
+++ Java-Terminal/src/JavaTerminal.java
@@ -1,8 +1,12 @@
 public class JavaTerminal {
     public static void main(String[] args) throws Exception {
         System.out.println("Oi, fui executado pelo terminal");
+        
+        
+
+  
+
     }
     
-
 
 }
--- Poo/carro/src/carro/Autodromo.java
+++ Poo/carro/src/carro/Autodromo.java
@@ -1,2 +1,18 @@
-package carro;public class Autodromo {
+package carro;
+
+public class Autodromo {
+public static void main(String[] args ) {
+    Carro jeep = new Carro();
+    jeep.setChassi("897897");
+    //jeep.ligar();
+
+    Moto z400 = new Moto();
+    z400.setChassi("467754");
+    //z400.ligar();
+
+    Veiculo coringa = z400;
+
+    coringa.ligar();
 }
+    }
+
